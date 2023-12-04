94f924a819c33f5d3ba8bd9bcd7c70e2c460e3d7
matchaism <60777705+macchanism@users.noreply.github.com>
2023-12-04 20:08:24 +0900

    modify for deploy 2

--- js/api.js
+++ js/api.js
@@ -56,7 +56,7 @@ function status(mode) {
 
 // フォームから入力された硬貨の数量に基づいて、APIを使用して最適な組み合わせを見つけ、結果を表示します。結果が正常でない場合、エラーステータスを表示します。
 async function main() {
-  const url = '160.251.12.126:5000/api';
+  const url = '//160.251.12.126:5000/api';
   //const url = 'http://localhost:5000/api';
   const coinValues = {
     '1': parseInt(document.getElementById('1yen').value),
