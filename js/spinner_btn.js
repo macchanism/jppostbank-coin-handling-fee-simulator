d7a272d730fd77ce0ad93f75307ff65808ff45c4
matchaism <60777705+matchaism@users.noreply.github.com>
2024-12-15 00:14:59 +0900

    Updates

--- js/spinner_btn.js
+++ js/spinner_btn.js
@@ -1,70 +1,58 @@
-$(function(){
+"use strict";
+$(function () {
     const spinnerCtrl = {
         interval: null,
         target: null,
         timestamp: 0,
-        cal: 0
+        cal: 0,
     };
-
-    const spinSpeed = 20; //変動スピード
-
+    const spinSpeed = 20; // 変動スピード
     // 長押し押下時
-    $('.btnspinner').on('touchstart mousedown click', function(e){
-        if(spinnerCtrl.interval) return false;
-
-        const target = $(this).data('target');
+    $(".btnspinner").on("touchstart mousedown click", function (e) {
+        if (spinnerCtrl.interval)
+            return false;
+        const target = $(this).data("target");
         spinnerCtrl.target = target;
         spinnerCtrl.timestamp = e.timeStamp;
-        spinnerCtrl.cal = Number($(this).data('cal'));
-
+        spinnerCtrl.cal = Number($(this).data("cal"));
         // クリックは単一の処理に留める
-        if(e.type == 'click'){
+        if (e.type === "click") {
             spinnerCal();
             resetSpinnerCtrl();
             return false;
         }
-
         // 長押し時の処理
-        setTimeout(function(){
+        setTimeout(() => {
             // インターバル未実行中 + 長押しのイベントタイプスタンプ一致時に計算処理
-            if(!spinnerCtrl.interval && spinnerCtrl.timestamp == e.timeStamp){
-                spinnerCtrl.interval = setInterval(spinnerCal, spinSpeed);
+            if (!spinnerCtrl.interval && spinnerCtrl.timestamp === e.timeStamp) {
+                spinnerCtrl.interval = window.setInterval(spinnerCal, spinSpeed);
             }
         }, 500);
     });
-
     // 長押し解除時 画面スクロールも解除に含む
-    $(document).on('touchend mouseup scroll', function(){
+    $(document).on("touchend mouseup scroll", () => {
         resetSpinnerCtrl();
     });
-
     // 変動計算関数
-    function spinnerCal(){
-        const target = $(spinnerCtrl.target);
-        let num = Number(target.val());
+    function spinnerCal() {
+        if (!spinnerCtrl.target)
+            return;
+        const targetElement = $(spinnerCtrl.target);
+        let num = Number(targetElement.val());
         num += spinnerCtrl.cal;
-
-        const maxVal = Number(target.data('max'));
-        const minVal = Number(target.data('min'));
-
+        const maxVal = Number(targetElement.data("max"));
+        const minVal = Number(targetElement.data("min"));
         num = Math.min(Math.max(num, minVal), maxVal);
-        target.val(num);
+        targetElement.val(num);
     }
-
     // スピナーコントロールリセット
-    function resetSpinnerCtrl(){
-        clearInterval(spinnerCtrl.interval);
+    function resetSpinnerCtrl() {
+        if (spinnerCtrl.interval) {
+            clearInterval(spinnerCtrl.interval);
+        }
         spinnerCtrl.interval = null;
         spinnerCtrl.target = null;
         spinnerCtrl.timestamp = 0;
         spinnerCtrl.cal = 0;
     }
 });
-
-
-/*
-下記サイトの上記開発者さまのソースコードを参考にしました．
-
-Developer: のちょう(https://kinocolog.com)
-from "https://kinocolog.com/spinner_btn/"
-*/
